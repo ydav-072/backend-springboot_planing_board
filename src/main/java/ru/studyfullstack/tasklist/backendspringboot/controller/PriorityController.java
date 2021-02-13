3d4199740f866d0f2758618d67968bb13c268628
admin <root>
2021-02-14 00:51:16 +0300

    добавлена проверка на пустые параметры и уникальный ID

--- src/main/java/ru/studyfullstack/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/studyfullstack/tasklist/backendspringboot/controller/PriorityController.java
@@ -1,5 +1,7 @@
 package ru.studyfullstack.tasklist.backendspringboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.studyfullstack.tasklist.backendspringboot.entity.PriorityEntity;
 import ru.studyfullstack.tasklist.backendspringboot.repo.PriorityRepository;
@@ -24,8 +26,17 @@ public class PriorityController {
     }
 
     @PostMapping("/add")
-    public PriorityEntity add(@RequestBody PriorityEntity priorityEntity){
-        return priorityRepository.save(priorityEntity);
+    public ResponseEntity add(@RequestBody PriorityEntity priorityEntity){
+        //проверка на обязательные параметры
+        //id must be unique
+        if(priorityEntity.getId() != null && priorityEntity.getId() !=0){
+            return new ResponseEntity("parametr id is must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        //if title is null
+        if(priorityEntity.getTitle() == null || priorityEntity.getTitle().trim().length() == 0 ){
+            return new ResponseEntity("parametr title is must be not null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priorityRepository.save(priorityEntity));
     }
 
 }
